5004f432a80ab7658c2bd461cccf6857dc6710d1
Dawid <dawid_owizyc@hotmail.com>
2018-11-02 12:59:15 +0100

    Add project files.

---
+++ LAB03Databas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LAB03Databas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
